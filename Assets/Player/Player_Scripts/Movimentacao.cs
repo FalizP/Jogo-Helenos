92c331be4e0d3205737334bf7cbc51c6f4cfaa8a
FalizPalma <luizalma16@gmail.com>
2023-04-16 21:26:34 -0300

    Adicionando Funcionalidades
    - Lógica de interação com portas usando a tecla <E>.
    - Lógica de portas trancadas
    - Inventário de chaves

--- Assets/Player/Player_Scripts/Movimentacao.cs
+++ Assets/Player/Player_Scripts/Movimentacao.cs
@@ -1,31 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
+
     private Vector2 moveDirection = Vector2.zero;
+    private PlayerControls playerControls;
     private InputAction move;
-    private Player_Controls playerControls;
+    private InputAction interacting;
 
     private bool isJumping = false;
+    private bool isInteracting = false;
     public float speed = 5f;
+
+    #region Inventário
+    private List<string> keys = new List<string>();
+    #endregion
 
     #region Funções de Inicialização
     private void Awake()
     {
-        playerControls = new Player_Controls();
+        playerControls = new PlayerControls();
     }
 
     private void OnEnable()
     {
         move = playerControls.Player.Move;
         move.Enable();
+
+        interacting = playerControls.Player.Interacting;
+        interacting.Enable();
     }
 
     private void OnDisable()
     {
         move.Disable();
+        interacting.Disable();
     }
     #endregion
 
@@ -45,6 +59,13 @@ public class PlayerController : MonoBehaviour
         return moveDirection;
     }
 
+    private bool InteractingWithDoorLogic()
+    {
+        isInteracting = interacting.IsPressed();
+        return isInteracting;
+    }
+
+    #region Detecção de Colisões
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.CompareTag("Ground"))
@@ -67,6 +88,28 @@ public class PlayerController : MonoBehaviour
         {
             move.Disable();
         }
+
+        if (collision.gameObject.CompareTag("Door"))
+        {
+            InteractingWithDoorLogic();
+        }
+
+        if (collision.gameObject.CompareTag("DoorWithKey"))
+        {
+            string doorKey;
+            if (collision.gameObject.TryGetComponent(out doorKey))
+            {
+                if (keys.Contains(doorKey))
+                {
+                    InteractingWithDoorLogic();
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
     }
 
     private void OnCollisionExit2D(Collision2D collision)
@@ -76,4 +119,5 @@ public class PlayerController : MonoBehaviour
             move.Enable();
         }
     }
+    #endregion
 }
