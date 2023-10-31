2c3bea5cd6f9657080f4a8632d1c78652c6a6da6
Luiz Palma <luizpalma16@gmail.com>
2023-10-31 16:57:48 -0300

    Desenhos

--- Assets/Colorblind Effect/Assets/Scripts/LineGenerator.cs
+++ Assets/Colorblind Effect/Assets/Scripts/LineGenerator.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LineGenerator : MonoBehaviour
 {
     public GameObject linePrefab;
 
     Line activeLine;
+
+    [SerializeField]
+    private string faseSaida;
 
     void Update()
     {
@@ -25,6 +29,11 @@ public class LineGenerator : MonoBehaviour
         {
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             activeLine.UpdateLine(mousePos);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(faseSaida);
         }
     }
 }
